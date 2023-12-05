3a6a459c5143b1d1a8276f7c6b505c21f1603e50
arata-honda-stb <arata.honda@stanby.inc>
2023-12-05 15:00:11 +0900

    Feat: httpリクエストを受けるようにする

--- main.go
+++ main.go
@@ -3,26 +3,32 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func main() {
+func getBreadCrumbs(w http.ResponseWriter, r *http.Request) {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6370",
 	})
 	ctx := context.Background()
-	result, err := rdb.Get(ctx, "breadcrumb/hondy").Result()
+	key := r.URL.Path[len("/breadcrumb/"):]
+	result, err := rdb.Get(ctx, "breadcrumb/"+key).Result()
 	if err != nil {
 		panic(err)
 	}
 	data := []map[string]string{}
-
 	err = json.Unmarshal([]byte(result), &data)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(data)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
 
+func main() {
+	http.HandleFunc("/breadcrumb/", getBreadCrumbs)
+	log.Fatal(http.ListenAndServe(":8089", nil))
 }
